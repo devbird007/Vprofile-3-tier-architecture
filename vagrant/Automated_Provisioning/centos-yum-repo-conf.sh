960b70da893bc3db95533e6c88fbc78d277a6d00
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-10-01 18:42:17 +0100

    Fix sed errors

--- vagrant/Automated_Provisioning/centos-yum-repo-conf.sh
+++ vagrant/Automated_Provisioning/centos-yum-repo-conf.sh
@@ -10,11 +10,11 @@ NEW_BASEURL="${OLD_BASEURL:1}"
 OLD_SOURCE="http://mirror.centos"
 NEW_SOURCE="http://vault.centos"
 
-cd /etc/yum.repos.d
+cd /etc/yum.repos.d/
 
-sudo sed -i 's/$OLD_MIRROR/$NEW_MIRROR/' CentOS-Base.repo
-sudo sed -i 's/$OLD_BASEURL/$NEW_BASEURL/' CentOS-Base.repo
-sudo sed -i 's/OLD_SOURCE/NEW_SOURCE/' CentOS-Base.repo
+sudo sed -i "s/$OLD_MIRROR/$NEW_MIRROR/" CentOS-Base.repo
+sudo sed -i "s/$OLD_BASEURL/$NEW_BASEURL/" CentOS-Base.repo
+sudo sed -i "s#$OLD_SOURCE#$NEW_SOURCE#" CentOS-Base.repo
 
 sudo yum update -y
 
