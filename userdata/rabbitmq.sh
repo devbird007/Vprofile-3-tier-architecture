350a4f63983bda2b7d3d33581b8b96a67ffeb39c
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-10-08 10:23:42 +0100

    Edit rabbitmq conf

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -1,144 +1,39 @@
 #!/bin/bash
 set -x
 
-
-# Update the server
+## Update and install general tools
 sudo yum update -y
-sudo yum install epel-release -y
 
-## Import necessary signing keys
-### primary RabbitMQ signing key
-sudo rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc'
-### modern Erlang repository
-sudo rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key'
-### RabbitMQ server repository
-sudo rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key'
+sudo yum install epel-release wget -y
 
+cd /tmp/
 
-## Configure the yum repositories
+## The following if-statements will install a version of erlang and rabbitmq working with centos9
+## It may not work for other OS or even other versions of centos
+## Check if erl(esl-erlang) exists, otherwise download it
+if [ -f "/usr/bin/erl" ]; then
+    echo "Erlang already exists. Skipping download."
+else
+    wget https://github.com/rabbitmq/erlang-rpm/releases/download/v26.2.5.3/erlang-26.2.5.3-1.el7.x86_64.rpm
 
-cat <<EOF > /etc/yum.repos.d/rabbitmq.repo
-# In /etc/yum.repos.d/rabbitmq.repo
+    sudo yum -y install erlang-26.2.5.3-1.el7.x86_64.rpm
+fi
 
-##
-## Zero dependency Erlang RPM
-##
+## Install rabbitmq
+if [ -f "/usr/sbin/rabbitmq-server" ]; then
+    echo "Rabbitmq already exists. Skipping download."
+else
 
-[modern-erlang]
-name=modern-erlang-el9
-# Use a set of mirrors maintained by the RabbitMQ core team.
-# The mirrors have significantly higher bandwidth quotas.
-baseurl=https://yum1.rabbitmq.com/erlang/el/9/$basearch
-        https://yum2.rabbitmq.com/erlang/el/9/$basearch
-repo_gpgcheck=1
-enabled=1
-gpgkey=https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key
-gpgcheck=1
-sslverify=1
-sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-metadata_expire=300
-pkg_gpgcheck=1
-autorefresh=1
-type=rpm-md
+    sudo rpm --import https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc
+    wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v4.0.2/rabbitmq-server-4.0.2-1.el8.noarch.rpm
 
-[modern-erlang-noarch]
-name=modern-erlang-el9-noarch
-# Use a set of mirrors maintained by the RabbitMQ core team.
-# The mirrors have significantly higher bandwidth quotas.
-baseurl=https://yum1.rabbitmq.com/erlang/el/9/noarch
-        https://yum2.rabbitmq.com/erlang/el/9/noarch
-repo_gpgcheck=1
-enabled=1
-gpgkey=https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key
-       https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc
-gpgcheck=1
-sslverify=1
-sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-metadata_expire=300
-pkg_gpgcheck=1
-autorefresh=1
-type=rpm-md
-
-[modern-erlang-source]
-name=modern-erlang-el9-source
-# Use a set of mirrors maintained by the RabbitMQ core team.
-# The mirrors have significantly higher bandwidth quotas.
-baseurl=https://yum1.rabbitmq.com/erlang/el/9/SRPMS
-        https://yum2.rabbitmq.com/erlang/el/9/SRPMS
-repo_gpgcheck=1
-enabled=1
-gpgkey=https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key
-       https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc
-gpgcheck=1
-sslverify=1
-sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-metadata_expire=300
-pkg_gpgcheck=1
-autorefresh=1
+    sudo yum install -y rabbitmq-server-4.0.2-1.el8.noarch.rpm
+fi
 
 
-##
-## RabbitMQ Server
-##
 
-[rabbitmq-el9]
-name=rabbitmq-el9
-baseurl=https://yum2.rabbitmq.com/rabbitmq/el/9/$basearch
-        https://yum1.rabbitmq.com/rabbitmq/el/9/$basearch
-repo_gpgcheck=1
-enabled=1
-# Cloudsmith's repository key and RabbitMQ package signing key
-gpgkey=https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key
-       https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc
-gpgcheck=1
-sslverify=1
-sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-metadata_expire=300
-pkg_gpgcheck=1
-autorefresh=1
-type=rpm-md
-
-[rabbitmq-el9-noarch]
-name=rabbitmq-el9-noarch
-baseurl=https://yum2.rabbitmq.com/rabbitmq/el/9/noarch
-        https://yum1.rabbitmq.com/rabbitmq/el/9/noarch
-repo_gpgcheck=1
-enabled=1
-# Cloudsmith's repository key and RabbitMQ package signing key
-gpgkey=https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key
-       https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc
-gpgcheck=1
-sslverify=1
-sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-metadata_expire=300
-pkg_gpgcheck=1
-autorefresh=1
-type=rpm-md
-
-[rabbitmq-el9-source]
-name=rabbitmq-el9-source
-baseurl=https://yum2.rabbitmq.com/rabbitmq/el/9/SRPMS
-        https://yum1.rabbitmq.com/rabbitmq/el/9/SRPMS
-repo_gpgcheck=1
-enabled=1
-gpgkey=https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key
-gpgcheck=0
-sslverify=1
-sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-metadata_expire=300
-pkg_gpgcheck=1
-autorefresh=1
-type=rpm-md
-EOF
-
-## Update package metadata
-dnf update -y
-
-
-## Install dependencies and our software
-sudo dnf install -y socat logrotate
-
-sudo dnf install -y erlang rabbitmq-server
+## Install dependencies
+sudo yum install -y socat logrotate
 
 ## Start and enable the service
 sudo systemctl start rabbitmq-server
@@ -148,7 +43,6 @@ sudo systemctl enable rabbitmq-server
 echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config
 
 ## Creating user with user privileges
-
 user_exists=$(sudo rabbitmqctl list_users | grep "test")
 
 if [[ -z "$user_exists" ]]; then
