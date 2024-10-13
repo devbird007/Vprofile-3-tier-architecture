cd7fe5890efd89f7972000106f5fb8c06d47916a
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-10-13 19:24:14 +0100

    Configure external access in the memcached script

--- vagrant/Automated_Provisioning/memcache.sh
+++ vagrant/Automated_Provisioning/memcache.sh
@@ -12,6 +12,9 @@ sudo systemctl enable memcached
 sudo systemctl start firewalld
 sudo systemctl enable firewalld
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
+sudo systemctl restart memcached
+
 firewall-cmd --add-port=11211/tcp
 firewall-cmd --add-port=11111/udp
 firewall-cmd --runtime-to-permanent
